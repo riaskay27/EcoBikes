172fcabe91093e1a5a878364e543971db3352b35
riaskay2708 <riaskay2708@ukr.net>
2020-06-05 15:10:44 +0300

    Create view methods

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,4 +1,23 @@
 package view;
 
 public class View {
+
+    public void printMenu(){
+
+        System.out.println(
+                "Please, make your choice:\n"+
+                "1-Show the entire EcoBike catalog\n"+
+                "2 – Add a new folding bike\n" +
+                "3 – Add a new speedelec\n" +
+                "4 – Add a new e-bike\n" +
+                "5 – Find the first item of a particular brand\n" +
+                "6 – Write to file\n" +
+                "7 – Stop the program"
+        );
+    }
+
+    public void printResult(String result){
+        System.out.println(result);
+    }
+
 }
